2ca89a27eae967fca4aa1f90022cf908a0c5026c
Tianjie(TJ) Zhang <95270677+TianjieZhang1993@users.noreply.github.com>
2021-12-13 23:09:36 -0700

    Update download.py

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 '''
 This file is used to download necessary datasets.
 '''
-
+import sys
 import dload
 # download Movielens
 dload.save_unzip("https://files.grouplens.org/datasets/movielens/ml-25m.zip")
